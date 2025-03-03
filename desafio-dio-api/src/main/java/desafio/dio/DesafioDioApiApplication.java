44e2e5ac1bf3fa303b6e32c8d643b2a76c60a537
Daniel <86085164+msck1@users.noreply.github.com>
2025-03-03 00:18:52 -0300

    Initial commit

---
+++ desafio-dio-api/src/main/java/desafio/dio/DesafioDioApiApplication.java
@@ -0,0 +1,13 @@
+package desafio.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioDioApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioDioApiApplication.class, args);
+	}
+
+}
